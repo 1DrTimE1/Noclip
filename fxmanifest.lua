ab2a04eedfd203ca48f20bcb55aa74434350e894
1DrTimE1 <drtime171@gmail.com>
2025-06-05 20:26:53 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'DittNamn'
+description 'Noclip med Discord-logg via webhook'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
